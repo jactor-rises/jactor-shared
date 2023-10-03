4421f8f8ff04cb5c55f08137cf3e920da81e5545
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2023-10-03 11:49:59 +0200

    dto/commands as @JvmRecord

--- shared/src/main/kotlin/com/github/jactor/shared/dto/bruker.kt
+++ shared/src/main/kotlin/com/github/jactor/shared/dto/bruker.kt
@@ -2,14 +2,14 @@ package com.github.jactor.shared.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 
+@JvmRecord
 @Schema(description = "Metadata for bruker")
 data class UserDto(
-    @Schema(description = "Identifikator") var id: Long? = null,
-
-    @Schema(description = "Epostadresse") var emailAddress: String? = null,
-    @Schema(description = "Persondata") var person: PersonDto? = null,
-    @Schema(description = "Brukernavn") var username: String? = null,
-    @Schema(description = "Brukerstatus") var userType: UserType = UserType.ACTIVE
+    @Schema(description = "Identifikator") val id: Long? = null,
+    @Schema(description = "Epostadresse") val emailAddress: String? = null,
+    @Schema(description = "Persondata") val person: PersonDto? = null,
+    @Schema(description = "Brukernavn") val username: String? = null,
+    @Schema(description = "Brukerstatus") val userType: UserType = UserType.ACTIVE
 )
 
 @Schema(description = "Metadata for ulike brukertyper")
@@ -21,25 +21,25 @@ enum class UserType {
     INACTIVE
 }
 
+@JvmRecord
 @Schema(description = "Metadata for en person")
 data class PersonDto(
-    @Schema(description = "Identifikator") var id: Long? = null,
-
-    @Schema(description = "Adressen til en person") var address: AddressDto? = null,
-    @Schema(description = "Beskrivelse") var description: String? = null,
-    @Schema(description = "Fornavn") var firstName: String? = null,
-    @Schema(description = "Internasjonal id (land & språk - ISO/IEC 15897)") var locale: String? = null,
-    @Schema(description = "Etternavn") var surname: String = ""
+    @Schema(description = "Identifikator") val id: Long? = null,
+    @Schema(description = "Adressen til en person") val address: AddressDto? = null,
+    @Schema(description = "Beskrivelse") val description: String? = null,
+    @Schema(description = "Fornavn") val firstName: String? = null,
+    @Schema(description = "Internasjonal id (land & språk - ISO/IEC 15897)") val locale: String? = null,
+    @Schema(description = "Etternavn") val surname: String = ""
 )
 
+@JvmRecord
 @Schema(description = "Metadata for en adresse")
 data class AddressDto(
-    @Schema(description = "Identifikator") var id: Long? = null,
-
-    @Schema(description = "Addresselinje 1") var addressLine1: String? = null,
-    @Schema(description = "Addresselinje 2") var addressLine2: String? = null,
-    @Schema(description = "Addresselinje 3") var addressLine3: String? = null,
-    @Schema(description = "Poststed") var city: String? = null,
-    @Schema(description = "Land") var country: String? = null,
-    @Schema(description = "Postnnummer") var zipCode: String? = null
+    @Schema(description = "Identifikator") val id: Long? = null,
+    @Schema(description = "Addresselinje 1") val addressLine1: String? = null,
+    @Schema(description = "Addresselinje 2") val addressLine2: String? = null,
+    @Schema(description = "Addresselinje 3") val addressLine3: String? = null,
+    @Schema(description = "Poststed") val city: String? = null,
+    @Schema(description = "Land") val country: String? = null,
+    @Schema(description = "Postnnummer") val zipCode: String? = null
 )
