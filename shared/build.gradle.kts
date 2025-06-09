b8d69246fbb85800c043439373d99976a1dcf380
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2025-06-09 14:56:30 +0200

    created shared-test
    fixed configuration. all projects use jactor-modules-kotlin-conventions.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,23 +1,15 @@
 group = "com.github.jactor-rises"
 description = "jactor::shared"
 
-val assertkVersion: String by project
-val junitPlatformVersion: String by project
-val mockkVersion: String by project
-val springBootVersion: String = "3.5.0"
+plugins {
+    id("jactor-modules-kotlin-conventions")
+}
+
 val springdocVersion: String by project
 
 dependencies {
     implementation("org.springdoc:springdoc-openapi-ui:$springdocVersion")
 
     // test
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:$assertkVersion")
-    testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("org.junit.platform:junit-platform-suite:$junitPlatformVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
-        exclude(group = "org.assertj", module = "assertj-core")
-        exclude(group = "org.junit", module = "junit")
-        exclude(group = "org.hamcrest")
-        exclude(group = "org.mockito")
-    }
+    testImplementation(project(":shared-test"))
 }
