40e6f7dd8fe19d1b3aa1a47e81ba09dd82b2deb9
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2025-06-17 10:30:03 +0200

    SpringBeanNames with 4 bean names on each line

--- shared/src/test/kotlin/com/github/jactor/shared/SpringBeanNamesTest.kt
+++ shared/src/test/kotlin/com/github/jactor/shared/SpringBeanNamesTest.kt
@@ -26,25 +26,19 @@ class SpringBeanNamesTest {
     }
 
     @Test
-    fun `should retrieve 1 line names for every 10 names given`() {
+    fun `should retrieve 1 line names for every 4 names given`() {
         springBeanNames.add("com.github.jactor.shared.FooBar0")
         springBeanNames.add("com.github.jactor.shared.FooBar1")
         springBeanNames.add("com.github.jactor.shared.FooBar2")
         springBeanNames.add("com.github.jactor.shared.FooBar3")
-        springBeanNames.add("com.github.jactor.shared.FooBar4")
-        springBeanNames.add("com.github.jactor.shared.FooBar5")
-        springBeanNames.add("com.github.jactor.shared.FooBar6")
-        springBeanNames.add("com.github.jactor.shared.FooBar7")
-        springBeanNames.add("com.github.jactor.shared.FooBar8")
-        springBeanNames.add("com.github.jactor.shared.FooBar9")
 
         assertThat(springBeanNames.names).isEqualTo(
-            listOf("FooBar0, FooBar1, FooBar2, FooBar3, FooBar4, FooBar5, FooBar6, FooBar7, FooBar8, FooBar9")
+            listOf("FooBar0, FooBar1, FooBar2, FooBar3")
         )
     }
 
     @Test
-    fun `should retrieve 2 line names if there is more than 10 names but less than 20`() {
+    fun `should retrieve 4 line names if there is more than 12 names but less than 16`() {
         springBeanNames.add("com.github.jactor.shared.FooBar0")
         springBeanNames.add("com.github.jactor.shared.FooBar1")
         springBeanNames.add("com.github.jactor.shared.FooBar2")
@@ -61,12 +55,12 @@ class SpringBeanNamesTest {
         springBeanNames.add("com.github.jactor.shared.FooBarD")
 
         assertThat(springBeanNames.names).assertAll {
-            it named "names" withSize 2
+            it named "names" withSize 4
 
-            it.firstOrNull() named "line 1" equalTo "FooBar0, FooBar1, FooBar2, FooBar3, FooBar4, FooBar5, FooBar6, " +
-                "FooBar7, FooBar8, FooBar9"
-
-            it.lastOrNull() named "line 2" equalTo "FooBarA, FooBarB, FooBarC, FooBarD"
+            it.firstOrNull() named "line 1" equalTo "FooBar0, FooBar1, FooBar2, FooBar3"
+            it.getOrNull(index = 1) named "line 2" equalTo "FooBar4, FooBar5, FooBar6, FooBar7"
+            it.getOrNull(index = 2) named "line 3" equalTo "FooBar8, FooBar9, FooBarA, FooBarB"
+            it.lastOrNull() named "line 4" equalTo "FooBarC, FooBarD"
         }
     }
 }
