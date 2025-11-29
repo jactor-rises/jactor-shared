a8d15debf454006c59244546f5b988f3ff67f284
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2025-11-29 14:37:02 +0100

    #145: jactor-modules-spring-library.gradle.kts - moved repositories to shared and shared-test modules

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 group = "com.github.jactor-rises"
 version = "2.0.x-SNAPSHOT"
 description = "jactor::shared"
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    mavenLocal()
+}
+
 val javaVersion = libs.versions.jvm.get().toInt()
 
 java {
