0e296d48c80ff283b4c295b2ddb20aca4383daff
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2026-02-01 17:28:42 +0100

    #2: a valid build with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,39 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktlint)
-    alias(libs.plugins.versions)
+    kotlin("jvm") version libs.versions.kotlin
+    `java-library`
 }
 
-description = "jactor::shared"
+group = "com.github.jactor-rises"
+version = "0.0.1-SNAPSHOT"
 
 dependencies {
     implementation(libs.springdoc.openapi.ui)
 
     testImplementation(libs.assertk)
-    testImplementation(libs.junit.platform.suite)
     testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.platform.suite)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.jvm.get().toInt())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
 }
